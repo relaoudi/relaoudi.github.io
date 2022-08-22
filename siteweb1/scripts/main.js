41ee7af488666f3c805ed9f456abcc6771e1df82
relaoudi <111768360+relaoudi@users.noreply.github.com>
2022-08-22 22:58:13 +0200

    Add files via upload

---
+++ siteweb1/scripts/main.js
@@ -0,0 +1,28 @@
+
+let myImage = document.querySelector('img');
+
+myImage.addEventListener('click', function() {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/space.png') {
+      myImage.setAttribute('src', 'images/robot2.png');
+    } else {
+      myImage.setAttribute('src', 'images/space.png');
+    }
+});
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Veuillez saisir votre nom.');
+    localStorage.setItem('nom', myName);
+    myHeading.textContent = 'Bienvenue ' + myName;
+  }
+  if (!localStorage.getItem('nom')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('nom');
+    myHeading.textContent = 'Bienvenue, ' + storedName;
+  }
+  myButton.addEventListener('click', function() {
+    setUserName();
+  });
